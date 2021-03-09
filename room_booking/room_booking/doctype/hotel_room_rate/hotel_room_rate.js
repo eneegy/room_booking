469b53125be8edc8c11f3f18fe586461378097a5
thanhd <thanhdn1989@gmail.com>
2021-03-09 10:06:24 +0700

    Hotel Room Rate

---
+++ room_booking/room_booking/doctype/hotel_room_rate/hotel_room_rate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, eneegy and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hotel Room Rate', {
+	// refresh: function(frm) {
+
+	// }
+});
