6a0284fea9fcb8b3bda437f9736a3512ecf36c67
Thanh Dao <thanhdn1989@gmail.com>
2021-03-21 14:54:23 +0700

    Room booking initial code

--- room_booking/public/js/room_service.js
+++ room_booking/public/js/room_service.js
@@ -1,20 +1,42 @@
-// async function getAvailableRoom() {
-//     var formData = $('#roomSearchForm').serializeArray().reduce(function(obj, item) {
-//         obj[item.name] = item.value;
-//         return obj;
-//     }, {});
-//     $.ajax({
-//         method: 'GET',
-//         url: '/room-search',
-//         data: {
-//             cmd: 'room_booking.www.room-search.index.get_available_rooms',
-//             from_date: formData.fromDate,
-//             to_date: formData.toDate,
-//             capacity: formData.capacity
-//         },
-//         dataType: 'json',
-//         success: function(response) {
-//             console.log(response);
-//         }
-//     })
-// }
+async function deserializeFormData(formSelector) {
+    return $(formSelector).serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+}
+
+async function bookRoom() {
+    var formData = $("#roomBookingForm").serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});;
+    var bookingDetails = {
+        first_name: formData["guestFirstName"],
+        middle_name: formData["guestMiddleName"],
+        last_name: formData["guestLastName"],
+        email: formData["guestEmail"],
+        phone_number: formData["guestPhoneNumber"],
+        address: {
+            address_line: formData["guestAddressLine"],
+            city: formData["guestCity"],
+            state: formData["guestState"]
+        },
+        booking_detail: {
+            check_in: formData["checkin"],
+            check_out: formData["checkout"],
+            room: ""
+        }
+    }
+    $.ajax({
+        method: 'POST',
+        url: 'api/method/room_booking.www.room-booking.index.book_room',
+        data: JSON.stringify(bookingDetails),
+        headers: {
+            'X-Frappe-CSRF-Token': formData._token
+        },
+        dataType: 'json',
+        success: function(response) {
+            console.log(response);
+        }
+    });
+}
---
+++ room_booking/www/room-booking/index.py
@@ -0,0 +1,51 @@
+import frappe
+import json
+
+def get_context(context):
+    return context
+
+
+@frappe.whitelist(allow_guest=True)
+def book_room():
+    booking_details = json.loads(frappe.request.data)
+    create_customer_if_not_exist(booking_details)
+    abc = 1
+
+def create_customer_if_not_exist(booking_details):
+    internal_customer = frappe.db.get_value(
+        'Customer',
+        {
+            'mobile_no': booking_details['phone_number'],
+            'email_id': booking_details['email']
+        }
+    )
+    if (not internal_customer):
+        contact = frappe.get_doc({
+                "doctype": "Contact",
+                "mobile_nos":[
+                    {
+                        "phone": booking_details['phone_number']
+                        # "is_primary_phone": True,
+                        # "is_primary_mobile_no": True
+                    }
+                ],
+                "email_ids": [
+                    {
+                        "email_id": booking_details['email']
+                        # "is_primary": True
+                    }
+                ],
+                "first_name": booking_details['first_name'],
+                "middle_name": booking_details['middle_name'],
+                "last_name": booking_details['last_name']
+            })
+        contact.insert()
+        # customer = frappe.get_doc({
+        #     "doctype": "customer",
+        #     "customer_name": '%s %s %s'%(booking_details['first_name'], booking_details['middle_name'], booking_details['last_name']),
+        #     "customer_type": "Individual",
+        #     "customer_group": "Individual",
+        #     "territory": "Vietnam",
+        #     "customer_primary_contact": contact
+        # })
+        # customer.insert(ignore_permissions=True)
