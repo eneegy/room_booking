187d474ffddff5ea030e3d5ca20eff38ce6dc70e
Thanh Dao <thanhdn1989@gmail.com>
2021-03-21 16:29:36 +0700

    Adjust create customer and contact flow

--- room_booking/www/room-booking/index.py
+++ room_booking/www/room-booking/index.py
@@ -20,19 +20,33 @@ def create_customer_if_not_exist(booking_details):
         }
     )
     if (not internal_customer):
+        customer = frappe.get_doc({
+            "doctype": "Customer",
+            "customer_name": '%s %s %s'%(booking_details['first_name'], booking_details['middle_name'], booking_details['last_name']),
+            "customer_type": "Individual",
+            "customer_group": "Individual",
+            "territory": "All Territories"
+        })
+        customer.insert(ignore_permissions=True)
         contact = frappe.get_doc({
                 "doctype": "Contact",
-                "mobile_nos":[
+                "phone_nos":[
                     {
-                        "phone": booking_details['phone_number']
-                        # "is_primary_phone": True,
-                        # "is_primary_mobile_no": True
+                        "phone": booking_details['phone_number'],
+                        "is_primary_phone": True,
+                        "is_primary_mobile_no": True
                     }
                 ],
                 "email_ids": [
                     {
-                        "email_id": booking_details['email']
-                        # "is_primary": True
+                        "email_id": booking_details['email'],
+                        "is_primary": True
+                    }
+                ],
+                "links": [
+                    {
+                        "link_doctype": "Customer",
+                        "link_name": '%s %s %s'%(booking_details['first_name'], booking_details['middle_name'], booking_details['last_name'])
                     }
                 ],
                 "first_name": booking_details['first_name'],
@@ -40,12 +54,3 @@ def create_customer_if_not_exist(booking_details):
                 "last_name": booking_details['last_name']
             })
         contact.insert()
-        # customer = frappe.get_doc({
-        #     "doctype": "customer",
-        #     "customer_name": '%s %s %s'%(booking_details['first_name'], booking_details['middle_name'], booking_details['last_name']),
-        #     "customer_type": "Individual",
-        #     "customer_group": "Individual",
-        #     "territory": "Vietnam",
-        #     "customer_primary_contact": contact
-        # })
-        # customer.insert(ignore_permissions=True)
