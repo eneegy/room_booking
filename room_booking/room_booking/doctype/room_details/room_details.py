847a4f3cfd21f7de97f62fb6e436274453e241d7
thanhd <thanhdn1989@gmail.com>
2021-03-22 14:25:08 +0700

    Room booking flow - Worked

--- room_booking/room_booking/doctype/room_details/room_details.py
+++ room_booking/room_booking/doctype/room_details/room_details.py
@@ -5,6 +5,41 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+import frappe.database
 
 class RoomDetails(Document):
-	pass
+	def calculate_price(self, from_date, to_date):
+		filters = {
+			'from_date': from_date,
+			'to_date': to_date,
+			'room_name': self.room_name
+		}
+		rate = frappe.db.sql("""
+			select
+				detail.name,
+				room_type.rate as basic_rate,
+				current_room_rate.rate
+			from
+				`tabRoom Details` detail
+				join `tabRoom Types` room_type
+				left join (
+					select
+						room_rate.room_type,
+						room_rate.rate
+					from
+						`tabHotel Room Rate` room_rate
+						inner join `tabPeriods` period on room_rate.period = period.periods_name
+					where
+						DATE(period.from_date) <= %(from_date)s
+						and DATE(period.to_date) >= %(to_date)s
+						and room_rate.status = 'Active'
+				) AS current_room_rate
+				on current_room_rate.room_type = room_type.type_name
+			where
+				detail.room_type = room_type.type_name
+				and detail.room_name = %(room_name)s
+		""", filters, as_dict=1)
+		applied_rate = rate[0]
+		if not applied_rate['rate']:
+			return applied_rate['basic_rate']
+		return applied_rate['rate']
--- room_booking/room_booking/doctype/room_orders/room_orders.py
+++ room_booking/room_booking/doctype/room_orders/room_orders.py
@@ -5,6 +5,43 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+import frappe
 
 class RoomOrders(Document):
-	pass
+	def validate(self):
+		if len(self.room) == 0:
+			msg = "Not allow to save order with empty room list"
+			frappe.throw(msg)
+
+	def before_insert(self):
+		self.set_check_in_out_date()
+		self.extra_total = self.calculate_extras()
+		self.net_total = self.extra_total + self.get_room_price()
+
+	def set_check_in_out_date(self):
+		check_in = []
+		check_out = []
+		for room in self.room:
+			check_in.append(room.check_in)
+			check_out.append(room.check_out)
+		self.check_in = min(check_in)
+		self.check_out = max(check_out)
+	
+	def get_room_price(self):
+		room_total = 0
+		for room in self.room:
+			room_detail = frappe.get_doc("Room Details", room.room)
+			room_total += room_detail.calculate_price(room.check_in, room.check_out)
+		return room_total
+	
+	def calculate_extras(self):
+		extras_total = 0
+		extra_names = []
+		extras = frappe.db.get_all('Room Extras', fields=['extra_name', 'rate'])
+		for extra in self.extras:
+			selected_extra = next(item for item in extras if item.extra_name == extra.room_extra)
+			if not selected_extra:
+				continue
+			extras_total += selected_extra.rate * extra.quantity
+		return extras_total
+		
